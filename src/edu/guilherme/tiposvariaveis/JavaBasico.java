f717de6270ef820d73e7b2317922df61d4820b55
guipmuller <guilhermepmuller@gmail.com>
2023-03-09 17:15:02 -0300

    Add tipos e variaveis

--- src/edu/guilherme/tiposvariaveis/JavaBasico.java
+++ src/edu/guilherme/tiposvariaveis/JavaBasico.java
@@ -1,5 +1,17 @@
 package edu.guilherme.tiposvariaveis;
 
 public class JavaBasico {
-    System.out.println("Hello world!");
+
+    public static void main(String[] args) {
+/*
+tipos primitivos de dados: int (4b), byte (1b),  
+short (2b), long (8b), float (4b), double,  
+boolean e char, sendo todos objetos
+String é uma classe
+tipo float precisa terminar com F ex.: 3.14F
+assim como o long precisa terminar com L
+para constantes utiliza-se o final, tudo uppercase
+ex: final double PI = 3.14; 
+*/        
+    }
 }
